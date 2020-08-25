70274f7a27b6a8e325142d138b61382a70651304
Nguyen Nguyen <nguyen.nguyen@punosmobile.com>
2020-08-25 23:50:12 +0300

    first commit

---
+++ app/src/main/java/com/example/sensorbased/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sensorbased
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var oddClick = true
+
+        button.setOnClickListener {
+            textview_small.text = if (oddClick) getString(R.string.small_text_odd) else getString(R.string.small_text_even)
+            oddClick = !oddClick
+        }
+    }
+}
