95f0e7f94bcc459697fbe5894d057af4f5d3d423
Nguyen Nguyen <nguyen.nguyen@punosmobile.com>
2020-08-26 10:59:36 +0300

    Done Snackbar

--- app/src/main/java/com/example/sensorbased/MainActivity.kt
+++ app/src/main/java/com/example/sensorbased/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +16,24 @@ class MainActivity : AppCompatActivity() {
         val availableSubtitles = listOf<String>("Oh, this is the first subtitle", "Oh, another subtitle")
 
         switch_text_button.setOnClickListener {
-            textview_subtitle.text = availableSubtitles[availableSubtitles.indices.random()]
+            val currentSubtitle = textview_subtitle.text
+            val upcomingSubtitle = availableSubtitles[availableSubtitles.indices.random()]
+            var snackbarMessage: String
+
+            if (currentSubtitle !== upcomingSubtitle) {
+                textview_subtitle.text = upcomingSubtitle
+                snackbarMessage = getString(R.string.snackbar_message_subtitle_change)
+            } else {
+                snackbarMessage = getString(R.string.snackbar_message_subtitle_inact)
+            }
+
+            Snackbar.make(
+                switch_text_button,
+                snackbarMessage,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(getString(R.string.action_ok)) { }
+                .show()
         }
 
         clear_text_button.setOnClickListener {
