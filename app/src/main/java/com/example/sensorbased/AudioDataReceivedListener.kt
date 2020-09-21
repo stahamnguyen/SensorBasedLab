9c2b0a2511ec0cc284343b25cc7a39ce19183951
Nguyen Nguyen <nguyen.nguyen@punosmobile.com>
2020-09-22 00:43:01 +0300

    done

---
+++ app/src/main/java/com/example/sensorbased/AudioDataReceivedListener.kt
@@ -0,0 +1,3 @@
+package com.example.sensorbased
+
+typealias AudioDataReceivedListener = (ShortArray) -> Unit
--- app/src/main/java/com/example/sensorbased/MainActivity.kt
+++ app/src/main/java/com/example/sensorbased/MainActivity.kt
@@ -1,21 +1,70 @@
 package com.example.sensorbased
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.MotionEvent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val shortByte = mutableListOf<Short>()
+    private val recordingThread by lazy {
+        Recorder {
+            shortByte.addAll(it.toList())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                1
+            )
+        } else {
+            setup()
+        }
+    }
 
-        var oddClick = true
-
-        button.setOnClickListener {
-            textview_small.text = if (oddClick) getString(R.string.small_text_odd) else getString(R.string.small_text_even)
-            oddClick = !oddClick
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setup()
+        } else {
+            finish()
+        }
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setup() {
+        button.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    recordingThread.startRecording()
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    recordingThread.stopRecording()
+                    Thread().run {
+                        Thread.sleep(500)
+                        val t = Player(
+                            shortByte.toShortArray()
+                        )
+                        runOnUiThread {
+                            t.startPlayback()
+                        }
+                    }
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
